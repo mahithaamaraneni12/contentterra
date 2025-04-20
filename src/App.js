b204fe0cfbc1e10fd29865230e0c7b51825130a5
Mahitha Amaraneni <mahithaamaraneni2002@gmail.com>
2025-04-20 16:49:09 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import RedditCard from './RedditCard';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://www.reddit.com/r/reactjs.json')
+      .then((res) => res.json())
+      .then((data) => {
+        setPosts(data.data.children);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container" style={{ maxWidth: '1280px', minHeight: '720px' }}>
+      {/* 👇 Customize or remove this header if needed */}
+      <h1 className="text-center my-4">Reddit reactjs Feed</h1>
+
+      <div className="row">
+        {posts.map((post, index) => (
+          <div className="col-md-6 col-lg-4 mb-4" key={index}>
+            <RedditCard data={post.data} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
